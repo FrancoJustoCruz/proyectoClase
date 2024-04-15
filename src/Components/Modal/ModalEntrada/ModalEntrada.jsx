264702a981ba2c27e8cd66243388eed697ddfa23
aron <aronsotomarino@gmail.com>
2024-04-15 11:50:22 -0500

    modal-entrada

--- src/Components/Modal/ModalEntrada/ModalEntrada.jsx
+++ src/Components/Modal/ModalEntrada/ModalEntrada.jsx
@@ -1,21 +1,19 @@
-import './ModalEntrada.css'
+import './ModalEntrada.css';
 
 function ModalEntrada({ isOpen, onClose, monto, descripcion, handleInputChange, handleSave }) {
   return (
     isOpen && (
-      <div className="modal">
-        <div className="modalContent">
+      <div className="modalEntrada">
+        <div className="modalEntradaContent">
           <span className="closeButton" onClick={onClose}>X</span>
           <h2>Entrada</h2>
-          <label htmlFor="monto">Monto:</label>
-          <br />
-          <input type="number" id="monto" name="monto" value={monto} onChange={handleInputChange} />
-          <br />
-          <label htmlFor="descripcion">Descripción:</label>
-          <br />
-          <textarea id="descripcion" name="descripcion" value={descripcion} onChange={handleInputChange} />
-          <br />
-          <button onClick={handleSave}>Guardar</button>
+          <form className='form-entrada'>
+            <label htmlFor="monto">Monto:</label>
+            <input type="number" id="monto" name="monto" value={monto} onChange={handleInputChange} />
+            <label htmlFor="descripcion">Descripción:</label>
+            <textarea id="descripcion" name="descripcion" value={descripcion} onChange={handleInputChange} />
+            <button onClick={handleSave}>Guardar</button>
+          </form>
         </div>
       </div>
     )
